28181be56506bd1e264b98f2eeb873f85f5b5d94
victor-vas <victorvasconcelos88@gmail.com>
2021-04-09 19:14:40 -0300

    created component Header

---
+++ src/components/Header/mock.ts
@@ -0,0 +1,9 @@
+import { HeaderProps } from '.';
+
+export default {
+  blogName: 'Victor Vasconcelos',
+  blogDescription:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+  logo: '/assets/images/victor.jpg',
+  showText: true,
+} as HeaderProps;
