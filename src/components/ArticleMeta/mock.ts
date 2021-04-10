db1b832f4b7a5cc075397e0ad9fbc39a983b34e7
victor-vas <victorvasconcelos88@gmail.com>
2021-04-10 20:12:52 -0300

    created component ArticleMeta

---
+++ src/components/ArticleMeta/mock.ts
@@ -0,0 +1,22 @@
+import { ArticleMetaProps } from '.';
+
+export default {
+  createdAt: '2021-04-02T18:48:09.246+00:00',
+  author: {
+    id: '123456789',
+    slug: 'victor-vasconcelos',
+    displayName: 'Victor Vasconcelos',
+  },
+  categories: [
+    {
+      id: 'daldlvpçfg4849',
+      displayName: 'Tech',
+      slug: 'tech',
+    },
+    {
+      id: 'lpglpasgmdk0304',
+      displayName: 'JS',
+      slug: 'javascript',
+    },
+  ],
+} as ArticleMetaProps;
